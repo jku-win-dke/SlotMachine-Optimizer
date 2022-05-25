cc464f223f256afea85d6f3b0a4bfc35100e584c
kevschuetz <kevin.hotz@gmx.at>
2022-05-25 14:35:34 +0200

    refactored simulated PE

--- src/main/java/at/jku/dke/slotmachine/optimizer/optimization/optaplanner/customimplementation/SimulatedPrivacyEngine.java
+++ src/main/java/at/jku/dke/slotmachine/optimizer/optimization/optaplanner/customimplementation/SimulatedPrivacyEngine.java
@@ -20,45 +20,60 @@ public class SimulatedPrivacyEngine<Solution_> implements NeighbourhoodEvaluator
         this.aboveThresholdMinimumSize = 3;
     }
 
+    /**
+     * Takes a list of candidates, evaluates them and returns the best candidate along with the fitness of it.
+     * @param candidates the candidates
+     * @return the best candidate and its score
+     */
     @Override
     public Map<Score, Solution_> getBestSolutionFromNeighbourhood(List<Solution_> candidates) {
-        var order = evaluateFlightPrioritizations((List<FlightPrioritization>)candidates);
-        var entry = order.entrySet().stream().findFirst();
+        // Get ordered flight prios
+        Map<HardSoftScore, List<FlightPrioritization>> orderedFlightPrioritizations = evaluateFlightPrioritizations((List<FlightPrioritization>)candidates);
+        var entry = orderedFlightPrioritizations.entrySet().stream().findFirst();
 
-        if(entry.isEmpty()) return null;
+        if(entry.isEmpty()) return Collections.emptyMap();
 
-        var map = new HashMap<Score, Solution_>();
-        map.put(entry.get().getKey(), (Solution_)entry.get().getValue().get(0));
-        return map;
+        // Return best and score
+        var resultMap = new HashMap<Score, Solution_>();
+        resultMap.put(entry.get().getKey(), (Solution_)entry.get().getValue().get(0));
+        return resultMap;
     }
 
+    /**
+     * Returns all candidates above the threshold and their average score.
+     * @param candidates the candidates
+     * @param threshold the threshold
+     * @return a map with the avg score and the candidates above the threshold.
+     */
     @Override
     public Map<Score, List<Solution_>> getCandidatesAboveThreshold(List<Solution_> candidates, double threshold) {
-        var orderedFlightPrioMap = evaluateFlightPrioritizations((List<FlightPrioritization>)candidates);
-        var highScore = orderedFlightPrioMap.entrySet().stream().findFirst().get().getKey();
-        var ordering = orderedFlightPrioMap.entrySet().stream().findFirst().get().getValue();
+        // get ordered flight-prios
+        Map<HardSoftScore, List<FlightPrioritization>> orderedFlightPrioMap = evaluateFlightPrioritizations((List<FlightPrioritization>)candidates);
+        HardSoftScore highScore = orderedFlightPrioMap.entrySet().stream().findFirst().get().getKey();
+        List<FlightPrioritization> orderedFlightPrioritizations = orderedFlightPrioMap.entrySet().stream().findFirst().get().getValue();
 
         List<FlightPrioritization> flightPrioritizationsAboveThreshold;
         double thresholdDecrement = 0.001;
         do{
-            var thresholdScore = highScore.getSoftScore() > 0 ? highScore.getSoftScore() * threshold : highScore.getSoftScore() * (1 + (1 - threshold));
-            flightPrioritizationsAboveThreshold = ordering.stream()
-                    .filter(flightPrio -> calculator.calculateScore(flightPrio).getSoftScore() > thresholdScore)
-                    .collect(Collectors.toList());
+            double thresholdDoubleScore = highScore.getSoftScore() > 0 ? highScore.getSoftScore() * threshold : highScore.getSoftScore() * (1 + (1 - threshold));
+            flightPrioritizationsAboveThreshold = orderedFlightPrioritizations.stream()
+                    .filter(flightPrio -> calculator.calculateScore(flightPrio).getSoftScore() > thresholdDoubleScore)
+                    .toList();
             threshold = threshold - thresholdDecrement;
         }while(flightPrioritizationsAboveThreshold.size() < aboveThresholdMinimumSize);
 
-        var averageScore = flightPrioritizationsAboveThreshold.stream()
+        double averageDoubleScore = flightPrioritizationsAboveThreshold.stream()
                 .mapToInt(flightPrio -> calculator.calculateScore(flightPrio).getSoftScore())
                 .average()
                 .getAsDouble();
-        var solutionsAboveThrehold = new ArrayList<Solution_>();
+
+        List<Solution_> solutionsAboveThrehold = new ArrayList<>();
         for(var r : flightPrioritizationsAboveThreshold ){
             solutionsAboveThrehold.add((Solution_)r);
         }
-        var map = new HashMap<Score, List<Solution_>>();
-        map.put(HardSoftScore.ofSoft((int)averageScore), solutionsAboveThrehold);
-        return map;
+        var resultMap = new HashMap<Score, List<Solution_>>();
+        resultMap.put(HardSoftScore.ofSoft((int)averageDoubleScore), solutionsAboveThrehold);
+        return resultMap;
     }
 
     @Override
@@ -89,13 +104,23 @@ public class SimulatedPrivacyEngine<Solution_> implements NeighbourhoodEvaluator
     /*
             Internal-data-representation evaluation
      */
-    public Map<HardSoftScore, List<FlightPrioritization>> evaluateFlightPrioritizations(List<FlightPrioritization> candidates){
+    private Map<HardSoftScore, List<FlightPrioritization>> evaluateFlightPrioritizations(List<FlightPrioritization> candidates){
         var map = new HashMap<HardSoftScore, List<FlightPrioritization>>();
         List<FlightPrioritization> sortedList = candidates
                 .stream()
                 .sorted(Comparator.comparing(calculator::calculateScore))
                 .collect(Collectors.toList());
         Collections.reverse(sortedList);
+
+        // TODO: switch to returning scores for all solutions to avoid redundandt score calculation
+        /*
+        Map<FlightPrioritization, Score> evaluatedFlightPrios = candidates.stream()
+                        .collect(Collectors.toMap(Function.identity(), calculator::calculateScore));
+
+        evaluatedFlightPrios.entrySet().stream()
+                .max((f1, f2)-> evaluatedFlightPrios.get(f1).compareTo(evaluatedFlightPrios.get(f2)))
+                        .get();
+         */
 
         map.put(calculator.calculateScore(sortedList.get(0)), sortedList);
         return map;
